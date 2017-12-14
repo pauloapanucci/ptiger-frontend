5ede511c6b2fea1ac96336e3f702ebd19f1c3eec
Paulo Alexandre Piornedo Panucci <pauloapanucci@Paulos-MacBook-Pro.local>
2017-12-14 20:40:08 -0200

    ultima chance

--- ptigerlib.c
+++ ptigerlib.c
@@ -5,10 +5,6 @@ extern "C" {
 #include <string.h>
 #include <math.h>
 
-void power_print(int a, int b){
-    printf("%d\n", pow(a, b));
-}
-
 void print_integer(int n) {
     printf("%d", n);
 }
@@ -24,31 +20,6 @@ void print_real(float n) {
 void println() {
     printf("\n");
 }
-
-void print_matrix_integer(int **m, int line, int column) {
-    for (int i = 0; i < line; i++) {
-        for (int j = 0; j < column; j++) {
-            printf(" %d ", m[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-void print_matrix_real(int **m, int line, int column) {
-    for (int i = 0; i < line; i++) {
-        for (int j = 0; j < column; j++) {
-            printf(" %f ", m[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-void print_array_integer(int a[], int size) {
-    for (int i = 0; i < size; i++) {
-        printf(" %d ", a[i]);
-    }
-}
-
 
 void print_hello_ptiger() {
     printf("HELLO PTIGER, NOW YOU CAN USE FUNC FROM EXTERNAL SHARED LIB!\n");
